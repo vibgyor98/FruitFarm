2cfd540d2e8e44975f65b4a2359a2e693d016861
Sourav Kar <karsourav05@gmail.com>
2020-04-18 18:18:32 +0530

    sending json

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const http = require('http');
 const fs = require('fs');
-
+const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-    res.end('Hello there, Welcome to FruitFarm!');
+    const pathName = req.url;
+    if(pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW');
+    } else if(pathName === '/product') {
+        res.end('This is the PRODUCT');
+    } else if(pathName === '/api') {
+        fs.readFile(`${__dirname}/devdata/data.json`, 'utf-8', (err, data) => {
+            const proData = JSON.parse(data);
+            res.writeHead(200, {'Content-type': 'application/json'});
+            res.end(data);
+        });
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html'
+        });
+        res.end('<h1>Page Not Found</h1>');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
